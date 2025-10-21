2849eaf12d76c59b3c945245ff310b61517b1563
_mitch.el <michele.gastaldi01@gmail.com>
2025-10-22 00:17:13 +0200

    Update offer descriptions and labels in offers.js

--- offers.js
+++ offers.js
@@ -1,10 +1,10 @@
 // CoolVoce Offers Data
 window.CoolVoceOffers = {
-  "EVO20_MNPANN": { desc: "📡 Minuti illimitati, 1000 SMS e 20GB in 4G+<br>💰 29.90€ primo anno, poi 4.90€/mese<b>💰 4.90€/mese (nessun costo iniziale)<br>📅 Scaduta nel: 12/2024" },
-  "EVO100_MNP4": { desc: "📱 Minuti illimitati, 1000 SMS e 100GB in 4G+<br>💰 5.50€/mese (attivazione gratuita)<br>📆 Scadenza: 22/10/2025" },
-  "EVO150_MNPGRT5": { desc: "📡 Minuti illimitati, 1000 SMS e 150GB in 4G+<br>💰 6.90€/mese (nessun costo iniziale)<br>📅 Scaduta nel: 07/2025" },
-  "EVO200_MNPGRT7": { desc: "📡 Minuti illimitati, 1000 SMS e 200GB in 4G+<br>💰 7.90€/mese (nessun costo iniziale)<br>📅 Scaduta nel: 12/2024" },
-  "EXT300MNPGRT5": { desc: "📡 Minuti illimitati, 1000 SMS e 300GB in 4G+<br>💰 7.90€/mese (attivazione gratuita)<br>📅 Scaduta nel: 06/2025" },
-  "EXT300MNPGRT4": { desc: "📡 Minuti illimitati, 1000 SMS e 300GB in 4G+<br>💰 9.90€/mese (nessun costo iniziale)<br>📅 Scaduta nel: 03/2025" },
-  "EVOUNMPATGRT": { desc: "📡 Minuti illimitati, 1000 SMS e GB Illimitati (form. 500) in 4G+<br>💰 9.90€/mese (attivazione gratuita)<br>📅 Scaduta nel: 11/2024" }
+  "EVO20_MNPANN": { label:"€2.49 EVO20 ANN", desc: "📱 MIN ILL, 1000 SMS e 20GB<br>💰 29.90€ 1° ANNO, poi 4.90€/mese (ATTIVAZIONE FREE)<br>📅 SCADENZA: 12/2024" },
+  "EVO100_MNP4": { label:"€5.50 EVO100", desc: "📱 MIN ILL, 1000 SMS e 100GB<br>💰 5.50€/mese (ATTIVAZIONE FREE)<br>📆 SCADENZA: 22/10/2025" },
+  "EVO150_MNPGRT5": { label:"€6.90 EVO150", desc: "📱 MIN ILL, 1000 SMS e 150GB<br>💰 6.90€/mese (NESSUN COSTO)<br>📅 SCADENZA: 07/2025" },
+  "EVO200_MNPGRT7": { label:"€7.90 EVO200", desc: "📱 MIN ILL, 1000 SMS e 200GB<br>💰 7.90€/mese (NESSUN COSTO)<br>📅 SCADENZA: 12/2024" },
+  "EXT300MNPGRT5": { label:"€7.90 EVO300", desc: "📱 MIN ILL, 1000 SMS e 300GB<br>💰 7.90€/mese (ATTIVAZIONE FREE)<br>📅 SCADENZA: 06/2025" },
+  "EXT300MNPGRT4": { label:"€9.90 EVO300", desc: "📱 MIN ILL, 1000 SMS e 300GB<br>💰 9.90€/mese (NESSUN COSTO)<br>📅 SCADENZA: 03/2025" },
+  "EVOUNMPATGRT": { label:"€9.90 EVO UNLIMITED", desc: "📱 MIN ILL, 1000 SMS e GB ILL (500GB) <br>💰 9.90€/mese (ATTIVAZIONE FREE)<br>📅 SCADENZA: 11/2024" }
 };
