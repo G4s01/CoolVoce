e3836ff360ecdaed1cbac3baba472d44292e6e2f
_mitch.el <michele.gastaldi01@gmail.com>
2025-10-22 00:23:52 +0200

    Add new offer EVO50 to offers data

--- offers.js
+++ offers.js
@@ -1,6 +1,7 @@
 // CoolVoce Offers Data
 window.CoolVoceOffers = {
   "EVO20_MNPANN": { label:"€2.49 EVO20 ANN", desc: "📱 MIN ILL, 1000 SMS e 20GB<br>💰 29.90€ 1° ANNO, poi 4.90€/mese (ATTIVAZIONE FREE)<br>📅 SCADENZA: 12/2024" },
+  "EVO50_MNPGRT2": { label:"€4.90 EVO50", desc: "📱 MIN ILL, 1000 SMS e 50GB<br>💰 4.90€/mese (NESSUN COSTO)<br>📆 SCADENZA: 12/2024" },
   "EVO100_MNP4": { label:"€5.50 EVO100", desc: "📱 MIN ILL, 1000 SMS e 100GB<br>💰 5.50€/mese (ATTIVAZIONE FREE)<br>📆 SCADENZA: 22/10/2025" },
   "EVO150_MNPGRT5": { label:"€6.90 EVO150", desc: "📱 MIN ILL, 1000 SMS e 150GB<br>💰 6.90€/mese (NESSUN COSTO)<br>📅 SCADENZA: 07/2025" },
   "EVO200_MNPGRT7": { label:"€7.90 EVO200", desc: "📱 MIN ILL, 1000 SMS e 200GB<br>💰 7.90€/mese (NESSUN COSTO)<br>📅 SCADENZA: 12/2024" },
